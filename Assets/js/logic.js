653722e4592f4b22b81dce38d0ee6cdadaea68a1
William-figure <wyc19981230@outlook.com>
2024-06-27 12:07:38 +1000

    add general attributes for background effect

---
+++ Assets/js/logic.js
@@ -0,0 +1,4 @@
+"use strict";
+const toggleCircleObj = document.querySelector("#toggle-circle");
+let currentTcColor = (toggleCircleObj.style.backgroundColor = "#000");
+const bodyObj = document.querySelector("body");
