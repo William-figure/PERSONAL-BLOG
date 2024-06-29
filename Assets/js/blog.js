40053688a4770e5f405bf5ba08640650f1516fd1
William-figure <wyc19981230@outlook.com>
2024-06-29 17:13:47 +1000

    remove unnecessary functionality

--- Assets/js/blog.js
+++ Assets/js/blog.js
@@ -11,18 +11,13 @@ const articleAuthor = JSON.parse(
 const articleDate = JSON.parse(localStorage.getItem("articleDate"));
 const postIt = document.querySelector("#post-target");
 const blogTitleObj = document.querySelector("#blog-page-title");
-const blogContainers = document.querySelectorAll(".article-container");
-
 toggleCircleObj.addEventListener("click", () => {
   if (currentTcColor == "#000") {
     toggleCircleObj.style.backgroundColor = "#fff";
     bodyObj.style.backgroundColor = "#000";
     bodyObj.style.color = "#fff";
-    blogTitleObj.style.color = "#fff";
+    blogTitleObj.style.color = "#3a3a3a";
     currentTcColor = "#fff";
-    blogContainers.forEach((blogContainer) => {
-      blogContainer.style.background = "linear-gradient(red, blue)";
-    })
   } else {
     toggleCircleObj.style.backgroundColor = "#000";
     bodyObj.style.backgroundColor = "#fff";
@@ -38,7 +33,7 @@ backBtnObj.addEventListener("click", () => {
 });
 for (let i = 0; i < articleTitle.length; i++) {
   postIt.innerHTML += `
-    <article article-${i} class="flex-container-column">
+    <article class="flex-container-column">
       <h2 class="article-title flex-item">${articleTitle[i]}</h2>
       <pre class="article-content flex-item">${articleContent[i]}</pre>
       <h6 class="article-author flex-item">${articleAuthor[i]} ${articleDate}</h6>
