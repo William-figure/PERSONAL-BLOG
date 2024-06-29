ac97538184530e2eb14f8978a35bd50c7539e525
William-figure <wyc19981230@outlook.com>
2024-06-29 11:44:26 +1000

    update the local storage with author date

--- Assets/js/form.js
+++ Assets/js/form.js
@@ -1,3 +1,4 @@
+"use strict";
 const titleObj = document.querySelector("#page-title");
 
 toggleCircleObj.addEventListener("click", () => {
@@ -20,16 +21,28 @@ const submitBtnObj = document.querySelector('#submit-btn');
 const userInputObj = document.querySelector('#username-input');
 const titleInputObj = document.querySelector('#title-input');
 const contentInputObj = document.querySelector('#content-input');
+const d = new Date();
+const userInput = [];
+const titleInput = [];
+const contentInput = [];
+const articleForm = {
+  articleAuthor: userInput,
+  articleTitle: titleInput,
+  articleContent: contentInput,
+  articleDate: `${d.getFullYear()}-${d.getMonth()+1}-${d.getDate()}`
+};
 submitBtnObj.addEventListener('click', (event) => {
   event.preventDefault();
-  let userInput = userInputObj.value;
-  let titleInput = titleInputObj.value;
-  let contentInput = contentInputObj.value;
-  if(userInput && titleInput && contentInput) {
-    localStorage.setItem('articleAuthor', userInput);
-    localStorage.setItem('articleTitle', titleInput);
-    localStorage.setItem('articleContent', contentInput);
-  } else {
+  if (userInputObj.value === ""|| titleInputObj.value === ""|| contentInputObj.value === "") {
     alert("Missing Value!");
+    return;
   }
+  confirm("Please double check before submission, if ready, please hit OK.")
+  userInput.push(userInputObj.value);
+  titleInput.push(titleInputObj.value);
+  contentInput.push(contentInputObj.value);
+  localStorage.setItem('articleAuthor', articleForm.articleAuthor);
+  localStorage.setItem('articleTitle', articleForm.articleTitle);
+  localStorage.setItem('articleContent', articleForm.articleContent);
+  localStorage.setItem('articleDate', articleForm.articleDate);
 })
